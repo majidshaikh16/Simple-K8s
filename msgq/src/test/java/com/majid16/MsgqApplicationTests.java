7ac334dc8bbe0331e6d8b60126c996f11dd22c74
Majid <majidshaikh16@gmail.com>
2019-10-15 14:54:38 +0530

    changes in directory

---
+++ msgq/src/test/java/com/majid16/MsgqApplicationTests.java
@@ -0,0 +1,16 @@
+package com.majid16;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MsgqApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
