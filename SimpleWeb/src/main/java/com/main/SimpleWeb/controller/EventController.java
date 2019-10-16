286a29eee547c7111b5a53ef1a84574fe1964fad
Majid <majidshaikh16@gmail.com>
2019-10-16 19:48:42 +0530

    solved some minor db prop issue

--- SimpleWeb/src/main/java/com/main/SimpleWeb/controller/EventController.java
+++ SimpleWeb/src/main/java/com/main/SimpleWeb/controller/EventController.java
@@ -1,12 +1,15 @@
 package com.main.SimpleWeb.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-@RestController("/userservice")
+@RestController
+@RequestMapping("/userservice")
 public class EventController {
     @PostMapping("/add")
     public String newOrder(@RequestBody String orderRequest) {
@@ -14,5 +17,9 @@ public class EventController {
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<String> responseEntity = restTemplate.postForEntity(eventUrl, orderRequest, String.class);
         return responseEntity.getBody() + " : " + responseEntity.getStatusCode();
+    }
+    @GetMapping("/test")
+    public String test() {
+    	return "test";
     }
 }
