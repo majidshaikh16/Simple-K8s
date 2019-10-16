d4341b28b758e4a460da6967c1689d1c28e9f482
Majid <majidshaikh16@gmail.com>
2019-10-16 21:32:37 +0530

    changed the rest call

--- SimpleWeb/src/main/java/com/main/SimpleWeb/controller/EventController.java
+++ SimpleWeb/src/main/java/com/main/SimpleWeb/controller/EventController.java
@@ -1,5 +1,12 @@
 package com.main.SimpleWeb.controller;
 
+import java.util.List;
+
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,8 +21,17 @@ public class EventController {
     @PostMapping("/add")
     public String newOrder(@RequestBody String orderRequest) {
         String eventUrl = "http://event-api-cluster-ip-service:8081/userservice/add/";
+        String url = "http://10.0.60.234:8086/add/order/details/";
+        System.out.println("request : "+orderRequest);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> request = new HttpEntity<>(orderRequest, headers);
+        
+        
+        
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> responseEntity = restTemplate.postForEntity(eventUrl, orderRequest, String.class);
+//        ResponseEntity<String> responseEntity = restTemplate.postForEntity(url, orderRequest, String.class);
+        ResponseEntity<String> responseEntity = restTemplate.exchange(eventUrl, HttpMethod.POST, request, new ParameterizedTypeReference<String>() { });
         return responseEntity.getBody() + " : " + responseEntity.getStatusCode();
     }
     @GetMapping("/test")
